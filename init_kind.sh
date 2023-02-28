ccbb03783b61d46883bf35906f922951226b07d3
EC2 Default User <ec2-user@ip-172-31-94-157.ec2.internal>
2023-02-28 04:54:44 +0000

    minor changes

--- init_kind.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git config --global user.name "amandhal"
-git config --global user.email "amandhal.ad@gmail.com"
-git config --global core.editor "vim"
-kind create cluster --config kind.yaml
